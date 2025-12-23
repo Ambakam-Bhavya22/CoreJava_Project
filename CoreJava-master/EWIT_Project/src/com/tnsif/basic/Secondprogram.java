8848c47198bb3d0ef3ea5b82fe6d40e347824d05
Ambakam-Bhavya22 <bhavyaprasanth22@gmail.com>
2025-12-23 21:26:06 +0530

    Add files via upload

---
+++ CoreJava-master/EWIT_Project/src/com/tnsif/basic/Secondprogram.java
@@ -0,0 +1,12 @@
+package com.tnsif.firstpackage;
+
+public class Secondprogram {
+
+	public static void main(String[] args) {
+		person p=new person();
+		p.display();
+		
+
+	}
+
+}
---
+++ EWIT_Project/src/com/tnsif/basic/Executor.java
@@ -0,0 +1,13 @@
+package com.tnsif.interfaces;
+
+public class Executor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//SampleInterface1 obj;
+				Derived d=new Derived();
+				d.display();
+			}
+		}
+
+	
